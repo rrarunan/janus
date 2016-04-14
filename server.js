6992f009c01456fe92e1181f95f0d693c8883a4f
Arunan Rabindran <arunan@docker.com>
2016-04-14 16:52:10 -0700

    Add koa, babel, protagonist and aglio

---
+++ server.js
@@ -0,0 +1,29 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import logger from 'koa-logger';
+import session from 'koa-session';
+import views from 'koa-views';
+import mount from 'koa-mount';
+import serve from 'koa-static';
+import convert from 'koa-convert';
+import finalHandler from './lib/finalHandler';
+import router from './router';
+
+const app = new Koa();
+
+app.use(finalHandler());
+app.use(views(`${__dirname}/views`, {
+  map: {
+    html: 'nunjucks',
+  },
+}));
+app.use(logger());
+app.use(mount('/static', serve('public')));
+app.use(bodyParser());
+app.keys = ['some secret hurr'];
+app.use(convert(session(app)));
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+export default app;
