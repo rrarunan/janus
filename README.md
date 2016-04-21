9dbdd6a3fa705838f1625cbad32aa6484f4c6005
Arunan Rabindran <arunan@docker.com>
2016-04-21 02:32:15 -0700

    Changes to readme and default router

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Janus
 
-A local dev api proxy server that any body can use to mock APIs
+![Janus](https://upload.wikimedia.org/wikipedia/commons/7/73/Janus_Bifrons_by_Adolphe_Giraldon.png)
+
+A local dev api proxy server that anybody can use to mock APIs
 
 ## Features
 
 * Koa based api server
 * API-Blueprint based mocking/documentation publishing/collaboration
-* Dockerized
 * API Proxy that a UI developer needs (need I say more)
--- router.js
+++ router.js
@@ -1,45 +1,7 @@
 import Router from 'koa-router';
 import req from 'requisition';
-import r from 'superagent';
 
-const CONTROLLER_HOST = process.env.CONTROLLER_HOST;
-const BASE_AUTH = process.env.UCP_API_AUTH; //This is dev only
 const router = new Router();
-
-const baseGet = async (ctx) => {
-  console.log(ctx.headers);
-  const resp = await req(`https://${CONTROLLER_HOST}${ctx.request.url}`)
-    .set('Authorization', ctx.headers.authorization)
-    .query(ctx.query);
-  // console.log(resp);
-  ctx.status = resp.response.statusCode;
-  ctx.status === 200 ? ctx.body = await resp.json() : ctx.body = resp.response.statusMessage;
-}
-
-router.post('/auth/login', async (ctx, next) => {
-  const auth = await req.post(`https://${CONTROLLER_HOST}/auth/login`)
-    .send(ctx.request.body);
-  console.log('Response ', auth.json());
-  ctx.body = await auth.json();
-});
-
-router.post('/auth/logout', async (ctx, next) => {
-  // console.log(ctx.request.body);
-  const auth = await req.post(`https://${CONTROLLER_HOST}/auth/logout`)
-    .send(ctx.request.body);
-});
-
-router.get('/api/:splat', async (ctx, next) => {
-  await baseGet(ctx);
-});
-
-router.get('/info', async (ctx, next) => {
-  await baseGet(ctx);
-});
-
-router.get('/:entities/json', async (ctx, next) => {
-  await baseGet(ctx);
-});
 
 router.get('/', async (ctx, next) => {
   ctx.body = 'janus';
